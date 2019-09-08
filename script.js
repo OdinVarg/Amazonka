7f9e712978bf7f10ae4e00c87e0eff3ab33fe829
OdinVarg <odin.varg.dos@gmail.com>
2019-09-08 20:31:04 +0300

    added lesson 3 - slider, adaptive

---
+++ script.js
@@ -0,0 +1,28 @@
+$('.review-cards').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          arrows: false
+        }
+    },
+        {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              autoplay: true,
+              autoplaySpeed: 2000,
+              arrows: false
+            }  
+        },
+    ]
+  });
